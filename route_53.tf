f23fcf89db5361d9e6a76ee02ebbf562f8488f7a
Kasai Coe <streamwest1629@gmail.com>
2022-05-28 05:24:31 +0000

    #3: dns周りを追加

--- route_53.tf
+++ route_53.tf
@@ -1,9 +1,9 @@
 locals {
-  primary = "kasai-san.dev"
+  primary_zone = "kasai-san.dev"
 }
 
 resource "aws_route53_zone" "primary" {
-  name = local.primary
+  name = local.primary_zone
   lifecycle {
     prevent_destroy = true
   }
@@ -13,21 +13,46 @@ resource "aws_route53_zone" "primary" {
 }
 
 resource "aws_acm_certificate" "primary" {
-  domain_name = "*.${local.primary}"
+  domain_name       = "*.${local.primary_zone}"
+  validation_method = "DNS"
   lifecycle {
     prevent_destroy = true
   }
 }
 
-output "primary_hostzone" {
-  value = aws_route53_zone.primary
+output "host_primary_zonename" {
+  value = aws_route53_zone.primary.name
 }
 
-output "primary_zoneid" {
+output "host_primary_zoneid" {
   value     = aws_route53_zone.primary.id
   sensitive = true
 }
 
-output "primary_certarn" {
+output "host_primary_certarn" {
   value = aws_acm_certificate.primary.arn
+}
+
+resource "aws_route53_record" "cert" {
+  for_each = {
+    for dvo in aws_acm_certificate.primary.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_zone.primary.id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 300
+}
+
+resource "aws_route53_record" "docs" {
+  zone_id = aws_route53_zone.primary.id
+  name    = "docs.${local.primary_zone}"
+  records = ["streamwest-1629.github.io"]
+  type    = "CNAME"
+  ttl     = 300
 }
