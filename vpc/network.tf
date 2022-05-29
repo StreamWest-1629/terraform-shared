6eade727ea3f02379029917784d4ef013a33c89a
Kasai Coe <streamwest1629@gmail.com>
2022-05-29 23:14:53 +0000

    #12: ネットワークを修正

--- vpc/network.tf
+++ vpc/network.tf
@@ -18,6 +18,12 @@ resource "aws_route_table" "public" {
   )
 }
 
+resource "aws_route" "public" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
 resource "aws_route_table_association" "public" {
   count          = length(var.cidr_public)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
